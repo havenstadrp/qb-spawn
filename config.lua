d8c8676b764adfeec4547e4735c64e080a47f79c
bedar89 <bedar89@gmail.com>
2021-08-11 20:03:02 +0000

    changed spawn locations

--- config.lua
+++ config.lua
@@ -3,45 +3,45 @@ QB = {}
 QB.Spawns = {
     ["legion"] = {
         coords = {
-            x = 195.17, 
-            y = -933.77, 
-            z = 29.7, 
-            h = 144.5 
+            x = 213.81, 
+            y = -808.92, 
+            z = 31.01, 
+            h = 31.01 
         },
         location = "legion",
-        label = "Legion Square",
+        label = "Blokkenpark Parking",
     },
 
     ["policedp"] = {
         coords = {
-            x = 428.23, 
-            y = -984.28, 
-            z = 29.76, 
-            h = 3.5 
+            x = 413.08, 
+            y = -1011.0, 
+            z = 29.35, 
+            h = 81.29 
         },
         location = "policedp",
-        label = "Police Department",
+        label = "Hoofd Politie Kantoor",
     },
 
     ["paleto"] = {
         coords = {
-            x = 80.35, 
-            y = 6424.12, 
-            z = 31.67, 
-            h = 45.5 
+            x = 1851.594,
+            y = 3670.896,
+            z = 33.787, 
+            h = 222.54
         },
         location = "paleto",
-        label = "Paleto Bay",
+        label = "Sandy Politie Kantoor",
     },
 
     ["motel"] = {
         coords = {
-            x = 327.56, 
-            y = -205.08, 
-            z = 53.08, 
-            h = 163.5 
+            x = -211.758,
+            y = 6308.203,
+            z = 31.423, 
+            h = 56.62 
         }, 
         location = "motel",
-        label = "Motels",
+        label = "Polito Ziekenhuis",
     },
 }
